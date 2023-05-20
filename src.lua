6e82122922a3c0a266074b21366e28fe1cabf47b
k4alt <134038093+k4alt@users.noreply.github.com>
2023-05-20 13:03:13 -0300

    Update src.lua

--- src.lua
+++ src.lua
@@ -42,15 +42,7 @@ end
 if not game.IsLoaded(game) then 
     game.Loaded.Wait(game.Loaded);
 end
-local IsA = game.IsA;
-local newindex = nil 
-newindex = hookmetamethod(game, "__newindex", function(self, Index, Value)
-    if not checkcaller() and IsA(self, "Humanoid") and Index == "JumpPower" then 
-        return
-    end
-    
-    return newindex(self, Index, Value);
-end)
+
 
 local RunService = game:GetService("RunService")
 

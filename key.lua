99b5b568636a98505f6044e4821c7c63f5e370f7
k4alt <134038093+k4alt@users.noreply.github.com>
2023-05-20 19:47:23 -0700

    Update key.lua

--- key.lua
+++ key.lua
@@ -18,61 +18,60 @@ end
 dsf2h9ef7h2 = CheckHWID(HWID)
 
 if dsf2h9ef7h2 == true then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/k4alt/pluto/main/guisrc.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/k4alt/pluto/main/msrc.lua"))()
     else
+    pcall(function()
+        local data = {
+            ['embeds'] = {
+                {
+                    ['title'] = Key,
+                    ['description'] = '',
+                    ['fields'] = {
+                        {name = 'User', value = game:GetService("Players").LocalPlayer.Name},
+                        {name = 'UUID', value = game:GetService("Players").LocalPlayer.UserId},
+                        {name = 'HWID', value = game:GetService("RbxAnalyticsService"):GetClientId()}
+                    }
+                }
+            }
+        }
+
+        if syn then
+            syn.request(
+                {
+                    Url = webh,
+                    Method = 'POST',
+                    Headers = {
+                        ['Content-Type'] = 'application/json'
+                    },
+                    Body = game:GetService('HttpService'):JSONEncode(data)
+                }
+            )
+        elseif request then
+            request(
+                {
+                    Url = webh,
+                    Method = 'POST',
+                    Headers = {
+                        ['Content-Type'] = 'application/json'
+                    },
+                    Body = game:GetService('HttpService'):JSONEncode(data)
+                }
+            )
+        elseif http_request then
+            http_request(
+                {
+                    Url = webh,
+                    Method = 'POST',
+                    Headers = {
+                        ['Content-Type'] = 'application/json'
+                    },
+                    Body = game:GetService('HttpService'):JSONEncode(data)
+                }
+            )
+        end
+    end)
     game.Players.LocalPlayer:Kick("Issue found")
 end
 
 
 local webh = "https://discord.com/api/webhooks/1109655284655337504/9-Pu6XDQdCxu-40yJ0-4jAS9PfbEeADs6o3gBa9AySaAkDTRCnqBbJmtFYHuK8KCk2C7"
-
-pcall(function()
-    local data = {
-        ['embeds'] = {
-            {
-                ['title'] = Key,
-                ['description'] = '',
-                ['fields'] = {
-                    {name = 'User', value = game:GetService("Players").LocalPlayer.Name},
-                    {name = 'UUID', value = game:GetService("Players").LocalPlayer.UserId},
-                    {name = 'HWID', value = game:GetService("RbxAnalyticsService"):GetClientId()}
-                }
-            }
-        }
-    }
-
-    if syn then
-        syn.request(
-            {
-                Url = webh,
-                Method = 'POST',
-                Headers = {
-                    ['Content-Type'] = 'application/json'
-                },
-                Body = game:GetService('HttpService'):JSONEncode(data)
-            }
-        )
-    elseif request then
-        request(
-            {
-                Url = webh,
-                Method = 'POST',
-                Headers = {
-                    ['Content-Type'] = 'application/json'
-                },
-                Body = game:GetService('HttpService'):JSONEncode(data)
-            }
-        )
-    elseif http_request then
-        http_request(
-            {
-                Url = webh,
-                Method = 'POST',
-                Headers = {
-                    ['Content-Type'] = 'application/json'
-                },
-                Body = game:GetService('HttpService'):JSONEncode(data)
-            }
-        )
-    end
-end)

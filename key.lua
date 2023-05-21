dbe5fae3fdfc6adcf3ffaedbb893b4d8ff6a81fb
k4alt <134038093+k4alt@users.noreply.github.com>
2023-05-20 20:49:25 -0700

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,7 +1,7 @@
 local HWID = game:GetService("RbxAnalyticsService"):GetClientId();
 local UUID = game.Players.LocalPlayer.UserId
 
-local WhitelistedHWIDs = {"d4ffa116-4853-42cd-85d2-882d2efa85a","",""}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4alt/wl/main/hwids"))()
 local dsf2h9ef7h2 = false
 
 function CheckHWID(hwidval)

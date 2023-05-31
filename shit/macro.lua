e919bdce864f6200c72d19a10dc2e1e613986317
k4alt <134038093+k4alt@users.noreply.github.com>
2023-05-30 18:14:54 -0700

    Update macro.lua

--- shit/macro.lua
+++ shit/macro.lua
@@ -2,7 +2,7 @@ local Player = game:GetService("Players").LocalPlayer
             local Mouse = Player:GetMouse()
             local SpeedGlitch = false
             Mouse.KeyDown:Connect(function(Key)
-                if Key == "q" then
+                if Key == Pluto.Macro.Keybind then
                     SpeedGlitch = not SpeedGlitch
                     if SpeedGlitch == true then
                         repeat game:GetService("VirtualInputManager"):SendMouseWheelEvent("0", "0", true, game)

1ccf466cc36bc4b8bbe3e26094cf8fa29f84a304
k4alt <134038093+k4alt@users.noreply.github.com>
2023-05-30 18:14:40 -0700

    Create macro.lua

---
+++ shit/macro.lua
@@ -0,0 +1,15 @@
+local Player = game:GetService("Players").LocalPlayer
+            local Mouse = Player:GetMouse()
+            local SpeedGlitch = false
+            Mouse.KeyDown:Connect(function(Key)
+                if Key == "q" then
+                    SpeedGlitch = not SpeedGlitch
+                    if SpeedGlitch == true then
+                        repeat game:GetService("VirtualInputManager"):SendMouseWheelEvent("0", "0", true, game)
+                                    wait(0.0000001)
+                                    game:GetService("VirtualInputManager"):SendMouseWheelEvent("0", "0", false, game)
+                                    wait(0.0000001)
+                        until SpeedGlitch == false
+                    end
+                end
+            end)
